b336ae672b773ab9e43330ff75b5d7aad6c999c7
Peter Csiba <petherz@gmail.com>
2023-11-10 09:52:31 -0800

    use sashabaranov/go-openai

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"encoding/json"
+	"context"
+	"errors"
 	"github.com/joho/godotenv"
-	"net/http"
+	"github.com/sashabaranov/go-openai"
+	"io"
 	"os"
 	"strings"
 	"time"
@@ -13,22 +14,9 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-type Choice struct {
-	Index        int                    `json:"index"`
-	Delta        map[string]interface{} `json:"delta"`
-	FinishReason *string                `json:"finish_reason"`
-}
-
-type EventData struct {
-	ID      string   `json:"id"`
-	Object  string   `json:"object"`
-	Created int64    `json:"created"`
-	Model   string   `json:"model"`
-	Choices []Choice `json:"choices"`
-}
-
 // Based off their Python version of the code https://cookbook.openai.com/examples/how_to_stream_completions
 // Translated with GPT-4: https://chat.openai.com/c/c723eeaa-2c24-42c2-aabb-0f5582d0f031
+// Using https://github.com/sashabaranov/go-openai/blob/d6f3bdcdac9172ab5248d6be8c3e1761446a434c/chat_stream.go#L62
 func main() {
 	// Initialize zerolog with console writer.
 	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
@@ -40,86 +28,53 @@ func main() {
 		log.Warn().Msgf("Cannot load .env file")
 	}
 	openAIAPIKey := os.Getenv("OPEN_AI_API_KEY")
+	if openAIAPIKey == "" {
+		log.Fatal().Msgf("OPEN_AI_API_KEY is not set")
+	}
+	client := openai.NewClient(openAIAPIKey)
 
-	client := &http.Client{}
 	startTime := time.Now()
 
-	// Construct the request body
-	reqBody := strings.NewReader(`{
-		"model": "gpt-3.5-turbo",
-		"messages": [
-			{"role": "user", "content": "Count to 100, with a comma between each number and no newlines. E.g., 1, 2, 3, ..."}
-		],
-		"temperature": 0,
-		"stream": true
-	}`)
+	// Create a chat completion request
+	chatRequest := openai.ChatCompletionRequest{
+		Model: "gpt-3.5-turbo",
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    "user",
+				Content: "Count to 100, with a comma between each number and no newlines. E.g., 1, 2, 3, ...",
+			},
+		},
+		Temperature: 0,
+	}
 
-	// Create and send the request
-	req, _ := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", reqBody)
-	req.Header.Add("Authorization", "Bearer "+openAIAPIKey)
-	req.Header.Add("Content-Type", "application/json")
-
-	// Send the request
-	resp, _ := client.Do(req)
-	defer resp.Body.Close()
-
-	// Use bufio.Reader to read the stream line by line
-	reader := bufio.NewReader(resp.Body)
+	// Create a chat completion stream
+	ctx := context.Background()
+	completionStream, createStreamErr := client.CreateChatCompletionStream(ctx, chatRequest)
+	if createStreamErr != nil {
+		log.Fatal().Msgf("Failed to create chat completion stream: %v", createStreamErr)
+	}
 
 	var contentBuilder strings.Builder
 
-	isDone := false
-	for !isDone {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			log.Error().Msgf("Error reading stream:", err)
+	for {
+		response, streamRecvErr := completionStream.Recv()
+
+		// Process the response
+		for _, choice := range response.Choices {
+			contentBuilder.WriteString(choice.Delta.Content)
+		}
+
+		chunkTime := time.Since(startTime)
+		log.Debug().Msgf("Data received %.2f seconds after request: %+v\n", chunkTime.Seconds(), response)
+
+		// We only handle the error at the end - since we can get io.EOF with the last token.
+		if streamRecvErr != nil {
+			if errors.Is(streamRecvErr, io.EOF) {
+				break // Stream closed, exit loop
+			}
+			log.Error().Msgf("Error reading from stream, closing: %v\n", streamRecvErr)
 			break
 		}
-
-		// Ignore empty lines and comments in the stream
-		if line == "\n" || strings.HasPrefix(line, ":") {
-			continue
-		}
-
-		// Handle event field (this seems to NOT happen)
-		if strings.HasPrefix(line, "event:") {
-			eventType := strings.TrimSpace(strings.TrimPrefix(line, "event:"))
-			log.Info().Msgf("Event type:", eventType)
-			continue
-		}
-
-		// Handle data field
-
-		if strings.HasPrefix(line, "data:") {
-			dataJSON := strings.TrimSpace(strings.TrimPrefix(line, "data:"))
-			var eventData EventData
-			if err := json.Unmarshal([]byte(dataJSON), &eventData); err != nil {
-				log.Error().Msgf("Error unmarshaling data:", err)
-				continue
-			}
-
-			// Iterate over choices and append content if it exists
-			for _, choice := range eventData.Choices {
-				if content, ok := choice.Delta["content"]; ok {
-					contentStr, ok := content.(string)
-					if !ok {
-						log.Error().Msgf("Content is not a string %+v", content)
-						continue
-					}
-					contentBuilder.WriteString(contentStr)
-				}
-				if choice.FinishReason != nil {
-					log.Info().Msgf("Response reading finished with " + *choice.FinishReason)
-					isDone = true
-				}
-			}
-
-			chunkTime := time.Since(startTime)
-			log.Info().Msgf("Data received %.2f seconds after request: %+v", chunkTime.Seconds(), eventData)
-			continue
-		}
-
-		log.Warn().Msgf("unhandled line: `%s`", line)
 	}
 
 	log.Info().Msgf("Full response received %.2f seconds after request", time.Since(startTime).Seconds())
